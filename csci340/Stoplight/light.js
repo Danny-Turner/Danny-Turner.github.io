4489b39fc0839b40a4cbe59179e31cc4a0ce04c4
Dan <turner@conwaycorp.net>
2019-10-25 10:01:05 -0500

    Simplified Code

--- csci340/Stoplight/light.js
+++ csci340/Stoplight/light.js
@@ -1,26 +1,19 @@
 $(document).ready(function (){
 
-//$("h1").html("testing");
-
-var lightColor = "RED";
 
 var stoplight = function() {
-  if (lightColor === "RED") {
+  if ($("h1").html() === "RED") {
     $("h1").html("GREEN");
     $("#light").css("background","#00ff00");
-    lightColor = "GREEN";
   }
-  else if (lightColor === "GREEN") {
+  else if ($("h1").html() === "GREEN") {
     $("h1").html("YELLOW");
     $("#light").css("background","yellow");
-    lightColor = "YELLOW";
   }
-  else if (lightColor === "YELLOW") {
+  else if ($("h1").html() === "YELLOW") {
     $("h1").html("RED");
     $("#light").css("background","#ff0000");
-    lightColor = "RED";
   }
-
 }
 
 var click = document.getElementById("light");
