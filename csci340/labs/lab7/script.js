448425093f3b5f6369d54b4f892a5b12ad3f9eb2
Dan Turner <turner@conwaycorp.net>
2019-10-29 17:22:45 -0500

    Formatting

--- csci340/labs/lab7/script.js
+++ csci340/labs/lab7/script.js
@@ -1,10 +1,23 @@
 $(document).ready(function (){
 
+  $.ajax({dataType: "json",
+  url: "https://api.magicthegathering.io/v1/sets/",
+  success: function(results) {
+    var sets = results;
+    console.log(sets);
+    console.log(sets["sets"].length);
 
 
-
-
-//var click = document.getElementById("light");
-//click.addEventListener("click", stoplight);
+    for (var i=0; i<sets["sets"].length ; i++) {
+      if (sets["sets"][i]["type"] == "core" || sets["sets"][i]["type"] =="expansion" || sets["sets"][i]["type"] =="masters") {
+          console.log(i+" "+sets["sets"][i]["name"]+" "+sets["sets"][i]["type"]);
+          $("p#setlist").append("<li>"+sets["sets"][i]["name"]+"</li>");
+        }
+    };
+  },
+  error: function(xhr,status,error) {
+    console.log(error);
+  }
+});
 
 });
