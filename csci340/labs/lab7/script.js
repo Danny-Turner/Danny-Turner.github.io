8780d7d09729bd6299effcbf8dfe2b28e34b4bdf
Dan <turner@conwaycorp.net>
2019-10-30 23:46:07 -0500

    NASA and drop down list

--- csci340/labs/lab7/script.js
+++ csci340/labs/lab7/script.js
@@ -3,60 +3,66 @@ $(document).ready(function (){
   $.ajax({dataType: "json",
     url: "https://api.magicthegathering.io/v1/sets/",
     success: function(results) {
-      createSetList(results);
-      setNameListener();
+      console.log(results);
+      setDateListener();
+      populateSetDropDown(results);
+      setDropDownListener();
     },
     error: function(xhr,status,error) {
       console.log(error);
     }
   });
 
-  function createSetList(sets) {
-    sets["sets"].sort(function(a,b){return new Date(a["releaseDate"]).getTime()-new Date(b["releaseDate"]).getTime()});
-    for (var i=0; i< sets["sets"].length ; i++) {
-      if (sets["sets"][i]["type"] == "core"
-        || sets["sets"][i]["type"] == "expansion"
-        || sets["sets"][i]["type"] == "masters") {
-        $("p#setlist").append("<li>"+sets["sets"][i]["name"]+"</li>");
-      };
-    };
+//      setDateListener();
 
+
+  function populateSetDropDown(sets) {
+    sets["sets"].sort(function(a,b){return new Date(b["releaseDate"]).getTime() - new Date(a["releaseDate"]).getTime()});
     for (var i=0; i< sets["sets"].length ; i++) {
-      if (sets["sets"][i]["type"] == "core"
+      if (sets["sets"][i]["name"].search("Renaissance") == -1
+        && sets["sets"][i]["name"].search("Rinascim") == -1
+        && sets["sets"][i]["name"].search("Summer Magic") == -1
+        && sets["sets"][i]["name"].search("Foreign") == -1
+        && sets["sets"][i]["onlineOnly"] == false
+        && (sets["sets"][i]["type"] == "core"
         || sets["sets"][i]["type"] == "expansion"
-        || sets["sets"][i]["type"] == "masters") {
-        $("#selectSet").append("<option value="+sets["sets"][i]["name"]+">"
+        || sets["sets"][i]["type"] == "masters")) {
+        $("#selectSet").append("<option value="+i+">"
         +sets["sets"][i]["name"]+"</option>");
       };
     };
-
+    $("selectSet>option:eq(1)").prop('selected', true);
   }
 
-  $("#selectSet").select(function(){
-    currentSet=$(this).text();
-    $("#setName").text(currentSet);
-    $("#cardlist li").remove();
-    $.ajax({dataType: "json",
-      url: "https://api.magicthegathering.io/v1/cards?setName="+currentSet,
-      success: function(results) {
-        createCardList(results, currentSet);
-        },
-      error: function(xhr,status,error) {
-        console.log(error);
-        }
-    });
-  });
 
-
-  function setNameListener(){
-    $("#setlist li").click(function(){
-      currentSet=$(this).text();
+  function setDropDownListener() {
+    $("#selectSet").on('change', function(){
+      var currentSet=$("#selectSet :selected").text();
+      console.log(currentSet);
       $("#setName").text(currentSet);
       $("#cardlist li").remove();
       $.ajax({dataType: "json",
         url: "https://api.magicthegathering.io/v1/cards?setName="+currentSet,
         success: function(results) {
           createCardList(results, currentSet);
+          },
+        error: function(xhr,status,error) {
+          console.log(error);
+          }
+      });
+    });
+  }
+
+  function setDateListener() {
+    $("#pickDate").on('change', function(){
+      var selectedDate= $("#pickDate").val();
+      console.log("https://api.nasa.gov/planetary/apod?api_key=tz5Behp5ZLCd6jUVH5m1rbfEmT4AN1g4LHlZnX6G & date="+selectedDate);
+      $.ajax({dataType: "json",
+        url: "https://api.nasa.gov/planetary/apod?api_key=tz5Behp5ZLCd6jUVH5m1rbfEmT4AN1g4LHlZnX6G&date="+selectedDate,
+        success: function(results) {
+          console.log(results);
+          $("#astronomyPic").attr("src",results["url"])
+          $("#astronomyDesc").text(results["explanation"])
           },
         error: function(xhr,status,error) {
           console.log(error);
@@ -79,7 +85,13 @@ $(document).ready(function (){
       $.ajax({dataType: "json",
         url: "https://api.magicthegathering.io/v1/cards?name="+$(this).text()+"&setName="+currentSet,
         success: function(results) {
-            $("#cardpic").attr("src",results["cards"][0]["imageUrl"])
+          console.log(results);
+          for (var i = 0; i < results["cards"].length; i++) {
+              console.log(results["cards"][i]["imageUrl"]);
+              if (results["cards"][i]["imageUrl"]!=null){
+                $("#cardpic").attr("src",results["cards"][i]["imageUrl"])
+              }
+            }
         },
         error: function(xhr,status,error) {
           console.log(error);
