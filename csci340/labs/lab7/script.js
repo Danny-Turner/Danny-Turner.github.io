291d7ad25ef21b9f3dfb1a9713be516d62f192bc
Dan Turner <turner@conwaycorp.net>
2019-10-31 12:20:10 -0500

    Cors...

--- csci340/labs/lab7/script.js
+++ csci340/labs/lab7/script.js
@@ -1,10 +1,13 @@
 $(document).ready(function (){
 
-  $.ajax({dataType: "json",
+  setDateListener();
+
+  $.ajax({crossOrign: true,
+    dataType: "json",
     url: "https://api.magicthegathering.io/v1/sets/",
+    context: {},
     success: function(results) {
       console.log(results);
-      setDateListener();
       populateSetDropDown(results);
       setDropDownListener();
     },
@@ -13,50 +16,10 @@ $(document).ready(function (){
     }
   });
 
-//      setDateListener();
-
-
-  function populateSetDropDown(sets) {
-    sets["sets"].sort(function(a,b){return new Date(b["releaseDate"]).getTime() - new Date(a["releaseDate"]).getTime()});
-    for (var i=0; i< sets["sets"].length ; i++) {
-      if (sets["sets"][i]["name"].search("Renaissance") == -1
-        && sets["sets"][i]["name"].search("Rinascim") == -1
-        && sets["sets"][i]["name"].search("Summer Magic") == -1
-        && sets["sets"][i]["name"].search("Foreign") == -1
-        && sets["sets"][i]["onlineOnly"] == false
-        && (sets["sets"][i]["type"] == "core"
-        || sets["sets"][i]["type"] == "expansion"
-        || sets["sets"][i]["type"] == "masters")) {
-        $("#selectSet").append("<option value="+i+">"
-        +sets["sets"][i]["name"]+"</option>");
-      };
-    };
-    $("selectSet>option:eq(1)").prop('selected', true);
-  }
-
-
-  function setDropDownListener() {
-    $("#selectSet").on('change', function(){
-      var currentSet=$("#selectSet :selected").text();
-      console.log(currentSet);
-      $("#setName").text(currentSet);
-      $("#cardlist li").remove();
-      $.ajax({dataType: "json",
-        url: "https://api.magicthegathering.io/v1/cards?setName="+currentSet,
-        success: function(results) {
-          createCardList(results, currentSet);
-          },
-        error: function(xhr,status,error) {
-          console.log(error);
-          }
-      });
-    });
-  }
 
   function setDateListener() {
     $("#pickDate").on('change', function(){
       var selectedDate= $("#pickDate").val();
-      console.log("https://api.nasa.gov/planetary/apod?api_key=tz5Behp5ZLCd6jUVH5m1rbfEmT4AN1g4LHlZnX6G & date="+selectedDate);
       $.ajax({dataType: "json",
         url: "https://api.nasa.gov/planetary/apod?api_key=tz5Behp5ZLCd6jUVH5m1rbfEmT4AN1g4LHlZnX6G&date="+selectedDate,
         success: function(results) {
@@ -72,12 +35,56 @@ $(document).ready(function (){
   }
 
 
+  function populateSetDropDown(sets) {
+    console.log("a"+sets);
+    sets["sets"].sort(function(a,b){return new Date(b["releaseDate"]).getTime() - new Date(a["releaseDate"]).getTime()});
+    for (var i=0; i< sets["sets"].length ; i++) {
+      if (sets["sets"][i]["name"].search("Renaissance") == -1
+        && sets["sets"][i]["name"].search("Rinascim") == -1
+        && sets["sets"][i]["name"].search("Summer Magic") == -1
+        && sets["sets"][i]["name"].search("Foreign") == -1
+        && sets["sets"][i]["onlineOnly"] == false
+        && (sets["sets"][i]["type"] == "core"
+        || sets["sets"][i]["type"] == "expansion"
+        || sets["sets"][i]["type"] == "masters")) {
+        $("#selectSet").append("<option value="+i+">"
+        +sets["sets"][i]["name"]+"</option>");
+      };
+    };
+  $("#selectSet").val("Core Set 2020");
+
+  }
+
+
+  function setDropDownListener() {
+    $("#selectSet").on('change', function(){
+      var currentSet=$("#selectSet :selected").text();
+      console.log(currentSet);
+      $("#setName").text(currentSet);
+      $("#instructions").text("(Select a Card for Details)");
+      $("#cardlist li").remove();
+      $.ajax({dataType: "json",
+        url: "https://api.magicthegathering.io/v1/cards?setName="+currentSet,
+        success: function(results) {
+          createCardList(results, currentSet);
+          },
+        error: function(xhr,status,error) {
+          console.log(error);
+          }
+      });
+    });
+  }
+
+
+
+
   function createCardList(cards, currentSet) {
     for (var i=0; i< cards["cards"].length ; i++) {
-        $("p#cardlist").append("<li>"+cards["cards"][i]["name"]+"</li>");
+        $("li#cardlist").append("<li>"+cards["cards"][i]["name"]+"</li>");
         };
     cardNameListener(currentSet);
   }
+
 
 
   function cardNameListener(currentSet) {
